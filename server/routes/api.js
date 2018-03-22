3563084bccba0a50f44247806a9f189d7d79e6f5
Di Benedetto <dibenedettom@dub-lp014431.pubedu.hegn.us>
2018-03-22 10:27:27 +0000

    added Events/Special routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -45,12 +45,38 @@ router.post('/login', (req, res) => {
                 res.status(401).send('invalid  email');
             } else if (user.password != userData.password) {
                 res.status(401).send('invalid  password');
-            }
-            else{
+            } else {
                 res.status(200).send(user);
             }
 
         })
 });
 
+router.get('/events', (req, res) => {
+    let events = [];
+    for (let i = 0; i < 50; i++) {
+        let event = {
+            _id: i + 1,
+            name: 'Auto',
+            desciption: 'Lorem ipsum',
+            date: '2018-04-20T18:25:43.511Z'
+        };
+        events.push(event);
+    }
+    res.json(events);
+});
+
+router.get('/special', (req, res) => {
+    let events = [];
+    for (let i = 0; i < 50; i++) {
+        let event = {
+            _id: i + 1,
+            name: 'Auto',
+            desciption: 'Lorem ipsum',
+            date: '2018-04-20T18:25:43.511Z'
+        };
+        events.push(event);
+    }
+    res.json(events);
+});
 module.exports = router;
